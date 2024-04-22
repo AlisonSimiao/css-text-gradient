e8e319de8a56943a644616a0ece1be19dcccd4c6
Alison Simião <alison.s@multiplier.com.br>
2024-04-22 18:23:58 -0300

    fix: add vite

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,4 @@
-class color {
+class Color {
     constructor(r, g, b) {
         this.r = r;
         this.g = g;
@@ -23,6 +23,18 @@ class color {
         const { r, g, b } = this;
         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
+
+    /**
+     * transforma o valor do hex em rgb
+     * @param {string} hex 
+     */
+    static hexTorgb(hex){
+        const r = parseInt(hex.slice(1, 3), 16);
+        const g = parseInt(hex.slice(3, 5), 16);
+        const b = parseInt(hex.slice(5, 7), 16);
+
+        return [r, g, b];
+    }
 }
 
 // Angle
@@ -32,40 +44,80 @@ const currentAngle = document.querySelector('.tool-angle .result');
 const updateAngle = (e) => {
     const resultAngle = e.target.value;
     currentAngle.textContent = resultAngle
+    updateTextResult();
 }
 
 rangeAngle.addEventListener('input', updateAngle)
 
 // Colors
 const colors = [
-    new color(255, 67, 89),
-    new color(9, 9, 9),
+    new Color(255, 0, 0),
+    new Color(0, 255, 0),
 ];
 
 const colorsContainer = document.querySelector('.tool-color .container');
-
-const updateColors = () => {
-    colorsContainer.innerHTML = '';
-    colors.forEach(color => {
-        colorsContainer.innerHTML += `
-                    <div class="color">
-                        <span class='button'>
-                            <i class="material-icons">back_hand</i>
-                        </span>
-                        <div class="screen-color" style="--i:${color.name}">
-                            ${color.name}
-                        </div>
-                        <span class='button'>
-                            <i class="material-icons">delete</i>
-                        </span>
-                    </div>
-        `;
-    });
+const changeColor = (event, color) => {
+    console.log(color)
+    const _color = event.target.value;
+    const newColor = new Color(Color.hexTorgb(color));
+    console.log(newColor)
+    //updateColors();
+    //updateTextResult();
 }
 
+const updateColors = () => {
+    
+       
+    colors.forEach((_color, index) => {
+        // container color
+        const containerColor = document.createElement('div')
+
+        containerColor.setAttribute('class', 'color')
+
+        //delete button
+        const dragButton = document.createElement('span')
+
+        dragButton.setAttribute('draggable', true)
+        dragButton.setAttribute('class', 'button')
+
+        dragButton.innerHTML =  '<i class="material-icons">back_hand</i>'
+
+        // color
+        const color = document.createElement('label')
+        
+        color.setAttribute('class', 'screen-color')
+        color.setAttribute('for', 'select-color')
+
+        color.style = `--i: ${_color.name}`
+        color.textContent = _color.name
+        
+        const inputColor = document.createElement('input')
+        
+        inputColor.setAttribute('type', 'color')
+        inputColor.setAttribute('id', 'select-color')
+        inputColor.addEventListener('input', (e) => changeColor(e, color))
+
+        color.appendChild(inputColor)
+
+        //delete button
+        const deleteButton = document.createElement('span')
+        
+        deleteButton.setAttribute('class', 'button')
+        deleteButton.innerHTML =  '<i class="material-icons">delete</i>'
+
+        containerColor.appendChild(dragButton)
+        containerColor.appendChild(color)
+        containerColor.appendChild(deleteButton)
+
+        colorsContainer.appendChild(containerColor)
+    });
+
+}
+
+// Css
 function createCssContent() {
     const css = `
-        background: linear-gradient(${currentAngle.textContent}deg, ${params.colors.map(color => color.name).join(', ')});
+        background: linear-gradient(${currentAngle.textContent}deg, ${colors.map(color => color.name).join(', ')});
         
         background-clip: text;
         -webkit-background-clip: text;
@@ -73,22 +125,36 @@ function createCssContent() {
         -webkit-text-fill-color: transparent;
         color: transparent;
     `
+    return css;
 }
+
 // tool text
 const text = document.querySelector('.tool-text input');
-const resultText = document.querySelector('.result-screen span');
+const resultText = document.querySelector('#result-screen span');
+
 
 const updateTextResult = () => {
-    console.log(text)
     resultText.textContent = text.value;
     const css = createCssContent();   
     resultText.style.cssText = css; 
-}
+} 
+
 text.addEventListener('input', updateTextResult);
+
+// tools colors
+
+const buttonAddColor = document.querySelector("#add-color span")
+
+const addColorEvent = () => {
+    document.queryzSelector("#add-color .modal")
+}
+
+buttonAddColor.addEventListener('click', addColorEvent);
 
 // Event initials
 const init = () => {
     updateColors();
+    updateTextResult();
 }
 
 window.onload = init;
